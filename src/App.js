f11c2a786da7e0d57b94b1626772e2f4cd7582a8
David <info@activenode.de>
2022-02-25 19:49:03 +0100

    feat: fetching all data (document) via graphql

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
+import { useQuery } from "@apollo/client";
+import { GET_DOCUMENT } from "./graphql/GET_DOCUMENT";
+
 function DocumentViewer() {
+  const { loading, error, data } = useQuery(GET_DOCUMENT, {
+    variables: { id: "e981971c-ff57-46dc-a932-a60dc1804992" },
+  });
+
+  console.log({ data });
+
   return <div>I will be the DocumentViewer Basis</div>;
 }
 
