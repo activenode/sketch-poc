0e45cf6a1b5f55077b3e720ba481fe1339a71faa
David <info@activenode.de>
2022-02-27 10:51:19 +0100

    feat: added simple listing of thumbnails in Artboard list

--- src/components/documents/ListArtboards.jsx
+++ src/components/documents/ListArtboards.jsx
@@ -1,3 +1,19 @@
-export const ListArtboards = () => {
-  return "Bring them Artboards on!";
+export const ListArtboards = ({ artboards }) => {
+  return artboards.map((artboard, i) => {
+    const files = artboard.files;
+    const lastFile = files[files.length - 1];
+    const { width, height } = lastFile;
+    const aspectRatio = width / height;
+    const thumbnails = lastFile.thumbnails;
+    const lastThumbnail = thumbnails[thumbnails.length - 1];
+    const { url } = lastThumbnail;
+
+    // using the url as key is probably better than the index.
+    // Why is there no artboard id?
+    return (
+      <div key={url}>
+        <img src={url} style={{ "aspect-ratio": aspectRatio.toFixed(3) }} />
+      </div>
+    );
+  });
 };
